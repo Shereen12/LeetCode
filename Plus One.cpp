a458c3227d53fbc03bfa45aa937cfc180d7023c1
Shereen <shereen12shereen@gmail.com>
2023-09-03 17:41:31 +0300

    Create Plus One.cpp

---
+++ Plus One.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int> ans;
+        int carry = 1;
+        for(int i = digits.size() - 1 ; i >= 0; i--){
+            if(digits[i] + carry > 9){
+                ans.push_back(0);
+                carry = 1;
+            }
+            else{
+                ans.push_back(digits[i] + carry);
+                carry = 0;
+            } 
+        }
+        if(carry == 1)
+            ans.push_back(1);
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
